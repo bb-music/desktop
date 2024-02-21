781a5f6629cc0ebf8fee349c6095f716a389a2aa
阿炸克斯 <975794403@qq.com>
2024-02-21 14:17:14 +0800

    feat: 新增搜索历史

--- frontend/src/app/modules/search/store.ts
+++ frontend/src/app/modules/search/store.ts
@@ -1,21 +1,32 @@
 import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
 import { SearchItem, SearchParams } from '@/app/api/search';
 import { api } from '@/app/api';
+import { JsonCacheStorage } from '@/app/lib/cacheStorage';
+
+const searchHistoryCache = new JsonCacheStorage<string[]>('bb-music-search-history');
 
 interface SearchStoreState {
   params: SearchParams;
   data: SearchItem[];
   loading: boolean;
+  history: string[];
 }
 interface SearchStoreHandler {
   setParams: (params: Partial<SearchParams>) => void;
   load: () => Promise<void>;
+  loadHistoryList: () => Promise<void>;
+  deleteHistory: (keyword: string) => Promise<void>;
+  clearHistory: () => Promise<void>;
 }
 
 type SearchStore = SearchStoreState & SearchStoreHandler;
 
 export const searchStore = create<SearchStore>()((set, get) => {
+  const loadHistoryList = () => {
+    return getHistoryList().then((res) => {
+      set({ history: res });
+    });
+  };
   return {
     params: {
       keyword: '',
@@ -23,6 +34,7 @@ export const searchStore = create<SearchStore>()((set, get) => {
     },
     data: [],
     loading: false,
+    history: [],
     load: async () => {
       const params = get().params;
       set({
@@ -30,6 +42,9 @@ export const searchStore = create<SearchStore>()((set, get) => {
       });
       try {
         const res = await api.search.getList(params);
+        appendHistory(params.keyword).then(() => {
+          loadHistoryList();
+        });
         set({
           data: res.list,
           loading: false,
@@ -50,7 +65,40 @@ export const searchStore = create<SearchStore>()((set, get) => {
         },
       }));
     },
+    loadHistoryList: async () => {
+      await loadHistoryList();
+    },
+    deleteHistory: async (name) => {
+      const res = await getHistoryList();
+      const list = res.filter((r) => r !== name);
+      await searchHistoryCache.set(list);
+      loadHistoryList();
+    },
+    clearHistory: async () => {
+      await searchHistoryCache.set([]);
+      loadHistoryList();
+    },
   };
 });
 
 export const useSearchStore = searchStore;
+
+async function getHistoryList() {
+  const res = (await searchHistoryCache.get()) || [];
+  return res;
+}
+
+async function appendHistory(name: string) {
+  const res = await getHistoryList();
+  let list = res.filter((r) => r !== name);
+  list.unshift(name);
+  if (list.length > 40) {
+    list = list.splice(0, 40);
+  }
+  await searchHistoryCache.set(list);
+}
+async function deleteHistory(name: string) {
+  const res = await getHistoryList();
+  const list = res.filter((r) => r !== name);
+  await searchHistoryCache.set(list);
+}
